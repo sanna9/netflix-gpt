fb210c87babaa86f886235d8f175c3e01bad1710
Sandeep K <sanna9aulakh@gmail.com>
2025-02-10 22:34:24 -0800

    movie-detail

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -16,7 +16,6 @@ const useMovieTrailer = (movieId) => {
     const filterTrailer = json.results.filter(
       (video) => video.type === "Trailer"
     );
-    debugger;
     const trailer = filterTrailer.length ? filterTrailer[0] : json.results[0];
 
     dispatch(addTrailerVideo(trailer));
